8156041525b55424e2263ee00dfabe6128d4deda
Victor Monzón Anglas <monzonanglasv@gmail.com>
2026-02-11 13:03:26 -0500

    Auto-generated commit

--- chunk-KX3FC5ET.js
+++
@@ -1,1 +0,0 @@
-var o={production:!0,apiUrl:"https://api.yourdomain.com/v1"};export{o as a};
---
+++ chunk-MZ3EZVY4.js
@@ -0,0 +1,1 @@
+var e={production:!0,apiUrl:"https://mediagenda-api-dev.azurewebsites.net/v1"};export{e as a};
